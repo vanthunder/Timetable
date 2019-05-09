06e0003f3886898d19176026299327085338161a
Lord Niklas <Lord Niklas@192.168.116.75>
2019-05-09 12:40:22 +0200

    Fünfter Commit Notiz-file mit Constructor und Variables

--- Timetable/src/Notiz/Notiz.java
+++ Timetable/src/Notiz/Notiz.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class Notiz extends Konstrukt {
 	
-	private boolean angeheftet;
-	private ArrayList angeheftetAn;
+	private int angeheftet;
+	private ArrayList<Termin> angeheftetAn;
 	private ArrayList externList;
 	private ArrayList fotoList;
 	private ArrayList gifList;
@@ -14,7 +14,14 @@ public class Notiz extends Konstrukt {
 	private ArrayList videoList;
 	
 	public Notiz(boolean angeheftet, ArrayList angeheftetAn, ArrayList externList, ArrayList fotoList, ArrayList gifList, ArrayList soundList, String textfeld, ArrayList videoList) {
-		
+		this.angeheftet = angeheftet;
+		this.angeheftetAn = angeheftetAn;
+		this.externList = externList;
+		this.fotoList = fotoList;
+		this.gifList = gifList;
+		this.soundList = soundList;
+		this.textfeld = textfeld;
+		this.videoList = videoList;
 	}
 
 }
