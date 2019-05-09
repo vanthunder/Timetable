5be4e73cda1045534098f87654269795dd5ad19e
Lord Niklas <Lord Niklas@192.168.116.75>
2019-05-09 12:15:14 +0200

    Vierter Commit just uploaded the Notiz-file

---
+++ Timetable/src/Notiz/Notiz.java
@@ -0,0 +1,20 @@
+package Notiz;
+
+import java.util.ArrayList;
+
+public class Notiz extends Konstrukt {
+	
+	private boolean angeheftet;
+	private ArrayList angeheftetAn;
+	private ArrayList externList;
+	private ArrayList fotoList;
+	private ArrayList gifList;
+	private ArrayList soundList;
+	private String textfeld;
+	private ArrayList videoList;
+	
+	public Notiz(boolean angeheftet, ArrayList angeheftetAn, ArrayList externList, ArrayList fotoList, ArrayList gifList, ArrayList soundList, String textfeld, ArrayList videoList) {
+		
+	}
+
+}
