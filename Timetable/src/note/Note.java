16e0f416b0cc48ad8aabef0fce910e2e456a1392
Melanpiriks <Melanpiriks@DESKTOP-JD450I1.fritz.box>
2019-05-12 13:41:59 +0200

    Achter Commit
    Getter/Setter hinzugefügt

--- Timetable/src/note/Note.java
+++ Timetable/src/note/Note.java
@@ -15,17 +15,83 @@ public class Note extends Base {
 	private String textbox;
 	private ArrayList videoList;
 	
+	public int getPinned() {
+		return pinned;
+	}
+
+	public void setPinned(int pinned) {
+		this.pinned = pinned;
+	}
+
+	public ArrayList getPinnedAt() {
+		return pinnedAt;
+	}
+
+	public void setPinnedAt(ArrayList pinnedAt) {
+		this.pinnedAt = pinnedAt;
+	}
+
+	public ArrayList getExternList() {
+		return externList;
+	}
+
+	public void setExternList(ArrayList externList) {
+		this.externList = externList;
+	}
+
+	public ArrayList getPhotoList() {
+		return photoList;
+	}
+
+	public void setPhotoList(ArrayList photoList) {
+		this.photoList = photoList;
+	}
+
+	public ArrayList getGifList() {
+		return gifList;
+	}
+
+	public void setGifList(ArrayList gifList) {
+		this.gifList = gifList;
+	}
+
+	public ArrayList getSoundList() {
+		return soundList;
+	}
+
+	public void setSoundList(ArrayList soundList) {
+		this.soundList = soundList;
+	}
+
+	public String getTextbox() {
+		return textbox;
+	}
+
+	public void setTextbox(String textbox) {
+		this.textbox = textbox;
+	}
+
+	public ArrayList getVideoList() {
+		return videoList;
+	}
+
+	public void setVideoList(ArrayList videoList) {
+		this.videoList = videoList;
+	}
+
 	public Note(String title, int pinned, ArrayList pinnedAt, ArrayList externList, ArrayList photoList, ArrayList gifList, ArrayList soundList, String textbox, ArrayList videoList) {
 		super(title);
 		this.title = title;
-		this.pinned = pinned;
-		this.pinnedAt = pinnedAt;
-		this.externList = externList;
-		this.photoList = photoList;
-		this.gifList = gifList;
-		this.soundList = soundList;
-		this.textbox = textbox;
-		this.videoList = videoList;
+		this.setPinned(pinned);
+		this.setPinnedAt(pinnedAt);
+		this.setExternList(externList);
+		this.setPhotoList(photoList);
+		this.setGifList(gifList);
+		this.setSoundList(soundList);
+		this.setTextbox(textbox);
+		this.setVideoList(videoList);
 	}
+	
+	
 
 }
