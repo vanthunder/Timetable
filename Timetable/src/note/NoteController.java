c282a2182b101a5e4fe7d9ecff754445d1554c25
Marvin <Marvin@Marvin.fritz.box>
2019-05-19 16:05:49 +0200

    Fifteenth Commit!
    +Adds a note view to the UI
    
    +Adds a button to the note view

---
+++ Timetable/src/note/NoteController.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package note;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+/**
+ * @author 
+ *
+ */
+public class NoteController implements Initializable
+{
+	@FXML
+	Button noteButton = new Button();
+	//Event for the link button
+	public void linkButtonPress(ActionEvent event)
+	{
+	//HIER KOMMT DIE BUTTON METHODE REIN!
+	}
+	//Initialize the NoteView with all its components
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
