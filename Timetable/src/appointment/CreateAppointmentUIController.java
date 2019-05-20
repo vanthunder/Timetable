f40aec9ddeb967dd47bf3404ed4abc5daa952df2
MADfx <MADfx@192.168.117.1>
2019-05-20 14:09:06 +0200

    Sechzehnter Commit (Marc und Niklas)
    CreateAppointmentUIController erstellt

---
+++ Timetable/src/appointment/CreateAppointmentUIController.java
@@ -0,0 +1,17 @@
+package appointment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class CreateAppointmentUIController {
+	
+	@FXML
+    Label createrLogLabel = new Label();
+	
+	public static void createAppointmentUI() {
+		if(floating == true && notesLink != null) {
+		
+		}
+	}
+
+}
