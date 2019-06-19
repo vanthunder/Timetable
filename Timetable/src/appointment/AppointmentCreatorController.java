b77332a31294e44101699f4d702c05f1ab111c3b
MADfx <MADfx@192.168.116.3>
2019-06-19 09:54:39 +0200

    Further Work an ApoointmentController Savebutton Method

--- Timetable/src/appointment/AppointmentCreatorController.java
+++ Timetable/src/appointment/AppointmentCreatorController.java
@@ -85,12 +85,18 @@ public class AppointmentCreatorController implements Initializable
 		//LocalDate save = AppointmentEndDate.getValue();
 		//CalendarController.addAppointment(save, AppointmentTitle.getText());
 		String title = AppointmentTitle.getText();
-		//LocalDateTime startpoint = LocalDateTime.of(AppointmentStartDate.getValue(), LocalTime.parse(AppointmentStartTime.getText(), Calendar.timeFormatter));
+		LocalDateTime startpoint = LocalDateTime.of(AppointmentStartDate.getValue(), LocalTime.parse(AppointmentStartTime.getText(), Calendar.timeFormatter));
 		LocalDateTime endpoint = LocalDateTime.of(AppointmentEndDate.getValue(), LocalTime.parse(AppointmentEndTime.getText(), Calendar.timeFormatter));
-		//boolean allDay = AppointmentAllDay.get
+		boolean allDay = AppointmentAllDay.getState();
 		LocalDateTime alarmTime = LocalDateTime.of(AppointmentAlarmDate.getValue(), LocalTime.parse(AppointmentAlarmTime.getText(), Calendar.timeFormatter));
+		boolean regularlyOnOff = AppointmentRegularlyOnOff.getState();
+		int regularlyType;
+		int regularlyAmount;
+		String description;
 		
-		//Creator.createAppointment(title, startpoint, endpoint, allDay, regularlyOnOff, regularlyType, regularlyAmount, description, alarmOnOff, alarmTime, notesPinned, notesLink, floating, chosenCategory);
+		
+		
+		Creator.createAppointment(title, startpoint, endpoint, allDay, regularlyOnOff, regularlyType, regularlyAmount, description, alarmOnOff, alarmTime, notesPinned, notesLink, floating, chosenCategory);
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources)
