ce42ef3025a7bdd71a856938461f2fdde5dca8ab
MADfx <MADfx@DESKTOP-QRS03GN.speedport.ip>
2019-06-24 19:25:15 +0200

    AppointmentCreatorController destroyed

--- Timetable/src/note/NoteOverviewInterface.java
+++
@@ -1,9 +0,0 @@
-package note;
-
-import java.util.ArrayList;
-
-
-public interface NoteOverviewInterface {
-	public ArrayList<Note> getNotesList();
-	public void setNotesList(ArrayList<Note> notesList);
-}
