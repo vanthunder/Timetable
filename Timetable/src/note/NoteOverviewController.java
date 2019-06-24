da77a7a97f189abf2fb15c348712f88025cae206
Marvin <Marvin@Marvin.fritz.box>
2019-06-24 17:44:03 +0200

    Forty-Sixth Commit!
    +Adds the possibility do display a note in the notes View!

--- Timetable/src/note/NoteOverviewController.java
+++ Timetable/src/note/NoteOverviewController.java
@@ -1,13 +1,31 @@
 package note;
 
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class NoteOverviewController implements Initializable
 {
-
+	@FXML
+	Button backButton;
+	@FXML
+	Button forwardButton;
+	@FXML
+	public void backButtonPress (ActionEvent event)
+	{
+		
+	}
+    @FXML
+	public void forwardButtonPress (ActionEvent event)
+	{
+		
+	}
+	
 	public int noteChoosing()
 	{
 		// TODO Auto-generated method stub
---
+++ Timetable/src/note/notesLabel.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package note;
+
+import javafx.scene.control.Label;
+
+/**
+ * @author Marvin
+ *
+ */
+public class notesLabel extends Label
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
